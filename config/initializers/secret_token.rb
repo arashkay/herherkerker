e2c6926b0d2160d202f13dafb20b285ed52a3007
Tectual <arash@tectual.com.au>
2013-09-05 13:08:09 +1000

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Herherkerker::Application.config.secret_token = '7361093455049ee47153957deffee11d7d7c684e2848d54631e153ff916753dce471d1fbfacb80a08df9d614b482b120daf9fcfc5a3770e4e7697c02a89bf8e5'
